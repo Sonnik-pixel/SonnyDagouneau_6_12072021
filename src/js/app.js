9403a3a0cd5db857f221e67e963a253a8f4417f5
Sonny <sonny.dagouneau@gmail.com>
2021-07-19 18:23:27 +0200

    start of the design on the figma model

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,11 @@
-
-
-// const url = fetch('http://127.0.0.1:5501/data.json')
-// const url = 'http://127.0.0.1:5501/data.json'
-// const response = await fetch('http://127.0.0.1:5501/data.json');
-// const commits = await response.json();
-// alert(commits[0].author.login);
-
-// .then(response => response.json())
-// .then(data => console.log(data))
-
-
+// Utilisation de fetch, je récupère mon fichier de données pour ensuite afficher le nom des photographes
 
 fetch('http://127.0.0.1:5501/data.json')
+    // retourne ensuite une promesse contenant la réponse (si tout se passe bien).
+    // On ne peut pas exploiter la réponse renvoyée dans cette promesse en l’état, il faut indiquer le format de réponse souhaité. Ici, on choisit JSON avec response.json().
     .then(response => response.json())
     .then(result => {
         // console.log(result.photographers[0].name)
-        // console.log(result.photographers[1].name)
         // for (var i = 0; i < result.photographers.length; i++) {
         //     console.log(result.photographers[i].name)
         //     console.log(result)
@@ -25,12 +15,5 @@ fetch('http://127.0.0.1:5501/data.json')
         });
     }
     )
-
-
-
-// fetch('http://127.0.0.1:5501/data.json')
-//     .then(response => response.json())
-//     .then(result => console.log(result.photographers[0].tags[0]))
-
-
-
+    // Permet d'afficher le type d'erreur rencontrée
+    .catch(error => alert("Il y a une erreur : " + error));
