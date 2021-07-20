5b21d471553f1a85089210741ec7e94c87896630
Sonny <sonny.dagouneau@gmail.com>
2021-07-20 15:55:59 +0200

    boucle done for display photographers name

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,8 @@ class Controller {
     async afficherListePhotographe() {
         let listPhotographe = await Model.get('http://127.0.0.1:5501/assets/data.json');
         console.log(listPhotographe);
-        //re faire la boucle ici
+        for (var i = 0; i < listPhotographe.photographers.length; i++) {
+            console.log(listPhotographe.photographers[i].name)
+        }
     }
 }
