889c2f8439b3c522b9518bfa38dc162e6db0e2b1
Sonny <sonny.dagouneau@gmail.com>
2021-08-13 15:59:43 +0200

    change path to data.json

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,7 @@
 class Controller {
     async afficherListePhotographe() {
         // Je récupère mes données
-        let listPhotographe = await Model.get('http://127.0.0.1:5501/assets/data.json');
+        let listPhotographe = await Model.get('https://sonnik-pixel.github.io/SonnyDagouneau_6_12072021/assets/data.json');
         console.log(listPhotographe);
         // permet de faire un affichage view.js
         let view = new View()
